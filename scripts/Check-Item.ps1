9a8a1b5a27de60c230b7b80f7fdcbefc57d11e82
Keith  Hudson <khudsonjd@users.noreply.github.com>
2026-03-18 21:20:58 -0700

    Replace ClientId with placeholder in script
    Updated the ClientId in the Connect-PnPOnline command to a placeholder.

--- scripts/Check-Item.ps1
+++ scripts/Check-Item.ps1
@@ -1,5 +1,5 @@
 $siteUrl='https://groovepoint.sharepoint.com/sites/Portfolio/'
-Connect-PnPOnline -Url $siteUrl -Interactive -ClientId '3dac4cee-ad25-4e62-a904-60d2cbc36c9b' -WarningAction SilentlyContinue
+Connect-PnPOnline -Url $siteUrl -Interactive -ClientId 'xxxxxx' -WarningAction SilentlyContinue
 $item = Get-PnPListItem -List 'TAPCatalog' -Id 2
 if ($item) {
     Write-Host 'Title:' $item.FieldValues.Title
